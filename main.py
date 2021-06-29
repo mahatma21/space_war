f67d6d10c60e916bded1eb47f5914d3d9a2691fd
Mahatma <79448225+mahatma21@users.noreply.github.com>
2021-06-29 21:11:05 +0800

    Update space war source code
    New update. Check space_war_v1.1 in release to see what's new.

--- main.py
+++ main.py
@@ -1,27 +1,288 @@
 import pygame
+from pygame.locals import *
 import sys
 import os
-import pyautogui
+import pyautogui  # To get window size
 
+
+class Player:
+    def __init__(
+            self, x: int, y: int,
+            border: pygame.Rect, facing: int,
+            img: pygame.Surface, color: tuple, key_binding: tuple):
+        self.rect = pygame.Rect(x, y, *PLAYER_SIZE)
+        self.border = border
+        self.color = color
+        self.facing = facing
+        self.health = PLAYER_HEALTH
+        self.update_health_text()  # Create health text
+        self.img = img
+        self.key_binding = key_binding
+        self.bullets = []
+
+    def handle_movement(self, keys_pressed):
+        # Move
+        if keys_pressed[self.key_binding[MOVE_LEFT]]:
+            self.rect.x -= PLAYER_VEL
+        if keys_pressed[self.key_binding[MOVE_RIGHT]]:
+            self.rect.x += PLAYER_VEL
+        if keys_pressed[self.key_binding[MOVE_UP]]:
+            self.rect.y -= PLAYER_VEL
+        if keys_pressed[self.key_binding[MOVE_DOWN]]:
+            self.rect.y += PLAYER_VEL
+        # Border
+        if self.rect.x < self.border.x:
+            self.rect.x = self.border.x
+        elif self.rect.x + self.rect.width > self.border.x + self.border.width:
+            self.rect.x = self.border.x + self.border.width - self.rect.width
+        if self.rect.y < self.border.y:
+            self.rect.y = self.border.y
+        elif self.rect.y + self.rect.height > self.border.y + self.border.height:
+            self.rect.y = self.border.y + self.border.height - self.rect.height
+
+    def handle_bullets(self, enemy):
+        for bullet in self.bullets:
+            bullet.x += BULLET_VEL * self.facing
+            # Collision
+            if enemy.rect.colliderect(bullet):
+                enemy.hit()
+                self.bullets.remove(bullet)
+            elif self.facing == 1:
+                if bullet.x > WIN_WIDTH:
+                    self.bullets.remove(bullet)
+            else:
+                if bullet.x + bullet.width < 0:
+                    self.bullets.remove(bullet)
+
+    def shoot(self):
+        # Play shoot sound effect
+        BULLET_FIRE_SOUND.play()
+        # Create bullet
+        if self.facing == 1:
+            self.bullets.append(
+                pygame.Rect(
+                    self.rect.x + self.rect.width, self.rect.y +
+                    (self.rect.height / 2) - (BULLET_HEIGHT / 2),
+                    *BULLET_SIZE))
+        else:
+            self.bullets.append(
+                pygame.Rect(
+                    self.rect.x - BULLET_WIDTH, self.rect.y +
+                    (self.rect.height / 2) - (BULLET_HEIGHT / 2),
+                    *BULLET_SIZE))
+
+    def hit(self):
+        # Play hit sound effect
+        BULLET_HIT_SOUND.play()
+        # Decrease player health
+        self.health -= 1
+        # re-render health text
+        self.update_health_text()
+
+    def draw_health_text(self, win: pygame.Surface):
+        # Draw health text
+        if self.facing == 1:
+            win.blit(self.health_text, (20, 20))
+        else:
+            win.blit(
+                self.health_text,
+                (WIN_WIDTH - self.health_text.get_width() - 20, 20))
+
+    def draw_spaceship(self, win: pygame.Surface):
+        # Draw player spaceship
+        win.blit(self.img, self.rect)
+
+    def draw_bullets(self, win: pygame.Surface):
+        # Draw player bullets
+        for bullet in self.bullets:
+            pygame.draw.rect(win, self.color, bullet)
+
+    def update_health_text(self):
+        if self.health > 3:
+            self.health_text = HEALTH_FONT.render(
+                f"Health: {self.health}", 1, WHITE)
+        else:
+            self.health_text = HEALTH_FONT.render(
+                f"Health: {self.health}", 1, RED)
+
+
+def redraw_window(blue: Player, red: Player):
+    # Draw background image
+    WIN.blit(BG, (0, 0))
+    # Draw player stuff
+    blue.draw_health_text(WIN)
+    blue.draw_spaceship(WIN)
+    blue.draw_bullets(WIN)
+
+    red.draw_health_text(WIN)
+    red.draw_spaceship(WIN)
+    red.draw_bullets(WIN)
+
+    pygame.display.update()
+
+
+def draw_winner(winner_text):
+    WIN.blit(
+        winner_text,
+        ((WIN_WIDTH / 2) - (winner_text.get_width() / 2),
+         (WIN_HEIGHT / 2) - (winner_text.get_height() / 2)))
+
+    pygame.display.update()
+
+    # Just allow QUIT event for game restart delay
+    pygame.event.set_allowed(QUIT)
+    # Delay before game restart
+    currTm = pygame.time.get_ticks()  # current time
+    startTm = currTm
+    # Game restart delay loop
+    while currTm - startTm < 3_000:  # 3 seconds before game restart
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+        # Preven CPU bound
+        pygame.time.wait(10)
+        # Update current time
+        currTm = pygame.time.get_ticks()
+
+
+def main():
+    clock = pygame.time.Clock()
+
+    blue = Player(*BLUE_STARTING_POINT, BLUE_BORDER, 1,
+                  BLUE_SPACESHIP, CYAN, BLUE_KEY_BINDING)
+
+    red = Player(*RED_STARTING_POINT, RED_BORDER, -1,
+                 RED_SPACESHIP, RED, RED_KEY_BINDING)
+
+    # Play background music
+    pygame.mixer.music.play(-1)
+
+    # Set allowed event to limit event loop
+    pygame.event.set_allowed((QUIT, KEYDOWN))
+
+    # Main loop
+    while 1:
+        # Set game FPS
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+
+            if event.type == KEYDOWN:
+                if event.key == blue.key_binding[SHOOT]:
+                    if len(blue.bullets) < MAX_BULLET:
+                        blue.shoot()
+
+                if event.key == red.key_binding[SHOOT]:
+                    if len(red.bullets) < MAX_BULLET:
+                        red.shoot()
+
+        keys_pressed = pygame.key.get_pressed()
+
+        # Handle players movement and bullets
+        blue.handle_movement(keys_pressed)
+        blue.handle_bullets(red)
+
+        red.handle_movement(keys_pressed)
+        red.handle_bullets(blue)
+
+        redraw_window(blue, red)
+
+        # Check for winner
+        if blue.health <= 0 or red.health <= 0:
+            if blue.health <= 0 and red.health <= 0:
+                winner_text = WINNER_FONT.render("Draw!", 1, WHITE)
+            elif blue.health <= 0:
+                winner_text = WINNER_FONT.render("Red Wins!", 1, RED)
+            elif red.health <= 0:
+                winner_text = WINNER_FONT.render("Blue Wins!", 1, CYAN)
+            # Stop background music
+            pygame.mixer.music.stop()
+            # Draw the winner
+            draw_winner(winner_text)
+            # Restart the game
+            main()
+
+
+# Colors
 CYAN = (0, 235, 235)
 RED = (235, 0, 0)
 WHITE = (235, 235, 235)
 
+# Main window
 WIN_HEIGHT = pyautogui.size()[1] - 200
 WIN_WIDTH = WIN_HEIGHT * 4 // 3
 WIN_SIZE = WIN_WIDTH, WIN_HEIGHT
-WIN = pygame.display.set_mode(WIN_SIZE)
+WIN_ICON = pygame.image.load(os.path.join('assets', 'icon.png'))
+# Set window caption and icon
 pygame.display.set_caption("Space War")
-pygame.display.set_icon((pygame.image.load(os.path.join('assets', 'icon.png'))))
+pygame.display.set_icon(WIN_ICON)
 
-MIDDLE_BORDER = pygame.Rect((WIN_WIDTH / 2) - 5, 0, 10, WIN_HEIGHT)
+# Player binding
+MOVE_LEFT, MOVE_RIGHT, MOVE_UP, MOVE_DOWN, SHOOT = [i for i in range(5)]
 
+""" How to get adaptive size
+If its width: WIN_WIDTH / default values of a attribute
+              times the result with WIN_WIDTH
+If its height: same but instead of WIN_WIDTH, use WIN_HEIGHT
+If its size: can use WIN_WIDTH or WIN_HEIGHT but i use WIN_WIDTH
+ """
+
+# Player stuff
+PLAYER_WIDTH = round(WIN_WIDTH / 14.58)
+PLAYER_HEIGHT = round(WIN_HEIGHT / 14.58)
+PLAYER_SIZE = PLAYER_WIDTH, PLAYER_HEIGHT
+PLAYER_VEL = round(WIN_WIDTH / 116.62, 2)
+PLAYER_HEALTH = 10
+
+BLUE_STARTING_POINT = (
+    (WIN_WIDTH * 0.25) - (PLAYER_WIDTH / 2),
+    (WIN_HEIGHT / 2) - (PLAYER_HEIGHT / 2))
+BLUE_BORDER = pygame.Rect(0, 0, (WIN_WIDTH / 2) - 10, WIN_HEIGHT)
+BLUE_KEY_BINDING = (K_a, K_d, K_w, K_s, K_LCTRL)
+
+RED_STARTING_POINT = (
+    (WIN_WIDTH * 0.75) - (PLAYER_WIDTH / 2),
+    (WIN_HEIGHT / 2) - (PLAYER_HEIGHT / 2))
+RED_BORDER = pygame.Rect((WIN_WIDTH / 2) + 10, 0,
+                         (WIN_WIDTH / 2) - 10, WIN_HEIGHT)
+RED_KEY_BINDING = (K_LEFT, K_RIGHT, K_UP, K_DOWN, K_RCTRL)
+
+# Player bullets
+BULLET_WIDTH = round(WIN_WIDTH / 77.75, 2)
+BULLET_HEIGHT = round(WIN_HEIGHT / 175.0, 2)
+BULLET_SIZE = BULLET_WIDTH, BULLET_HEIGHT
+BULLET_VEL = 2 * PLAYER_VEL
+MAX_BULLET = 3
+
+# Sounds
+pygame.mixer.init(48000)
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('assets', 'laser.wav'))
+BULLET_FIRE_SOUND.set_volume(0.2)
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('assets', 'explosion.wav'))
+BULLET_HIT_SOUND.set_volume(0.16)
+pygame.mixer.music.load(os.path.join('assets', 'backsound.wav'))
+pygame.mixer.music.set_volume(0.2)
+
+# Fonts
+pygame.font.init()
+HEALTH_FONT = pygame.font.Font(
+    os.path.join('assets', 'pixel_font.ttf'),
+    round(WIN_WIDTH / 23.32))
+WINNER_FONT = pygame.font.Font(
+    os.path.join('assets', 'pixel_font.ttf'),
+    round(WIN_WIDTH / 9.33))
+
+# Game fps
+FPS = 60
+
+# Create main window
+WIN = pygame.display.set_mode(WIN_SIZE)
+
+# Import images
 BG = pygame.transform.smoothscale(pygame.image.load(
     os.path.join('assets', 'bg.png')).convert(), WIN_SIZE)
-
-PLAYER_SIZE = PLAYER_WIDTH, PLAYER_HEIGHT = 64, 48
-PLAYER_VEL = 8
-PLAYER_HEALTH = 10
 
 BLUE_SPACESHIP = pygame.transform.smoothscale(pygame.transform.rotate(
     pygame.image.load(
@@ -38,221 +299,6 @@ RED_SPACESHIP = pygame.transform.smoothscale(pygame.transform.rotate(
             'red_spaceship.png')).convert_alpha(),
     270),
     PLAYER_SIZE)
-
-BULLET_SIZE = BULLET_WIDTH, BULLET_HEIGHT = 12, 4
-BULLET_VEL = 2 * PLAYER_VEL
-MAX_BULLET = 3
-
-# Sounds
-pygame.mixer.init(48000)
-BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('assets', 'laser.wav'))
-BULLET_FIRE_SOUND.set_volume(0.2)
-BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('assets', 'explosion.wav'))
-BULLET_HIT_SOUND.set_volume(0.16)
-pygame.mixer.music.load(os.path.join('assets', 'backsound.wav'))
-pygame.mixer.music.set_volume(0.2)
-
-BLUE_HIT = pygame.USEREVENT + 1
-RED_HIT = pygame.USEREVENT + 2
-
-# FONT
-pygame.font.init()
-HEALTH_FONT = pygame.font.Font(os.path.join('assets', 'pixel_font.ttf'), 40)
-WINNER_FONT = pygame.font.Font(os.path.join('assets', 'pixel_font.ttf'), 100)
-
-FPS = 60
-
-
-def draw_winner(winner_text):
-    WIN.blit(
-        winner_text,
-        ((WIN_WIDTH / 2) - (winner_text.get_width() / 2),
-         (WIN_HEIGHT / 2) - (winner_text.get_height() / 2)))
-
-    pygame.display.update()
-
-    for _ in range(300):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        pygame.time.delay(10)
-
-
-def handle_bullets(blue, red, blue_bullets, red_bullets):
-    for blue_bullet in blue_bullets:
-        blue_bullet.x += BULLET_VEL
-        # Collision
-        if red.colliderect(blue_bullet):
-            pygame.event.post(pygame.event.Event(RED_HIT))
-            blue_bullets.remove(blue_bullet)
-        elif blue_bullet.x > WIN_WIDTH:
-            blue_bullets.remove(blue_bullet)
-
-    for red_bullet in red_bullets:
-        red_bullet.x -= BULLET_VEL
-        # Collision
-        if blue.colliderect(red_bullet):
-            pygame.event.post(pygame.event.Event(BLUE_HIT))
-            red_bullets.remove(red_bullet)
-        elif red_bullet.x < 0:
-            red_bullets.remove(red_bullet)
-
-
-def red_handle_movement(red, keys_pressed):
-    # Move
-    if keys_pressed[pygame.K_LEFT]:  # LEFT
-        red.x -= PLAYER_VEL
-    if keys_pressed[pygame.K_RIGHT]:  # RIGHT
-        red.x += PLAYER_VEL
-    if keys_pressed[pygame.K_UP]:  # UP
-        red.y -= PLAYER_VEL
-    if keys_pressed[pygame.K_DOWN]:  # DOWN
-        red.y += PLAYER_VEL
-    # Border
-    if red.x < MIDDLE_BORDER.x + MIDDLE_BORDER.width:  # LEFT
-        red.x = MIDDLE_BORDER.x + MIDDLE_BORDER.width
-    elif red.x + red.width > WIN_WIDTH:  # RIGHT
-        red.x = WIN_WIDTH - red.width
-    if red.y < 0:  # UP
-        red.y = 0
-    elif red.y + red.height > WIN_HEIGHT:  # DOWN
-        red.y = WIN_HEIGHT - red.height
-
-
-def blue_handle_movement(blue, keys_pressed):
-    # Move
-    if keys_pressed[pygame.K_a]:  # LEFT
-        blue.x -= PLAYER_VEL
-    if keys_pressed[pygame.K_d]:  # RIGHT
-        blue.x += PLAYER_VEL
-    if keys_pressed[pygame.K_w]:  # UP
-        blue.y -= PLAYER_VEL
-    if keys_pressed[pygame.K_s]:  # DOWN
-        blue.y += PLAYER_VEL
-    # Border
-    if blue.x < 0:  # LEFT
-        blue.x = 0
-    elif blue.x + blue.width > MIDDLE_BORDER.x:  # RIGHT
-        blue.x = MIDDLE_BORDER.x - blue.width
-    if blue.y < 0:  # UP
-        blue.y = 0
-    elif blue.y + blue.height > WIN_HEIGHT:  # DOWN
-        blue.y = WIN_HEIGHT - blue.height
-
-
-def redraw_window(
-    blue, red, blue_bullets, red_bullets, blue_health_text,
-        red_health_text):
-    WIN.blit(BG, (0, 0))
-    WIN.blit(blue_health_text, (20, 20))
-    WIN.blit(red_health_text, (WIN_WIDTH - red_health_text.get_width() - 20, 20))
-    WIN.blit(BLUE_SPACESHIP, blue)
-    WIN.blit(RED_SPACESHIP, red)
-    # Draw bullets
-    for blue_bullet in blue_bullets:
-        pygame.draw.rect(WIN, CYAN, blue_bullet)
-    for red_bullet in red_bullets:
-        pygame.draw.rect(WIN, RED, red_bullet)
-
-    pygame.display.update()
-
-
-def main():
-    clock = pygame.time.Clock()
-
-    blue = pygame.Rect(
-        (WIN_WIDTH * 0.25) - (PLAYER_WIDTH / 2),
-        (WIN_HEIGHT / 2) - (PLAYER_HEIGHT / 2),
-        *PLAYER_SIZE)
-    blue_bullets = []
-    blue_health = PLAYER_HEALTH
-    blue_health_text = HEALTH_FONT.render(f"Health: {blue_health}", 1, WHITE)
-
-    red = pygame.Rect(
-        (WIN_WIDTH * 0.75) - (PLAYER_WIDTH / 2),
-        (WIN_HEIGHT / 2) - (PLAYER_HEIGHT / 2),
-        *PLAYER_SIZE)
-    red_bullets = []
-    red_health = PLAYER_HEALTH
-    red_health_text = HEALTH_FONT.render(f"Health: {red_health}", 1, WHITE)
-
-    # Play background music
-    pygame.mixer.music.play(-1)
-
-    pygame.event.set_allowed((pygame.QUIT, pygame.KEYDOWN, BLUE_HIT, RED_HIT))
-
-    # Main loop
-    while 1:
-        clock.tick(FPS)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            if event.type == pygame.KEYDOWN:
-
-                if event.key == pygame.K_LCTRL:
-                    if len(blue_bullets) < MAX_BULLET:
-                        blue_bullets.append(
-                            pygame.Rect(
-                                blue.x + blue.width, blue.y +
-                                (blue.height / 2) - (BULLET_HEIGHT / 2),
-                                *BULLET_SIZE))
-                        BULLET_FIRE_SOUND.play()
-
-                if event.key == pygame.K_RCTRL:
-                    if len(red_bullets) < MAX_BULLET:
-                        red_bullets.append(
-                            pygame.Rect(
-                                red.x - BULLET_WIDTH, red.y +
-                                (red.height / 2) - (BULLET_HEIGHT / 2),
-                                *BULLET_SIZE))
-                        BULLET_FIRE_SOUND.play()
-
-            if event.type == BLUE_HIT:
-                BULLET_HIT_SOUND.play()
-                blue_health -= 1
-                if blue_health > 3:
-                    blue_health_text = HEALTH_FONT.render(
-                        f"Health: {blue_health}", 1, WHITE)
-                else:
-                    blue_health_text = HEALTH_FONT.render(
-                        f"Health: {blue_health}", 1, RED)
-
-            if event.type == RED_HIT:
-                BULLET_HIT_SOUND.play()
-                red_health -= 1
-                if red_health > 3:
-                    red_health_text = HEALTH_FONT.render(
-                        f"Health: {red_health}", 1, WHITE)
-                else:
-                    red_health_text = HEALTH_FONT.render(
-                        f"Health: {red_health}", 1, RED)
-
-        keys_pressed = pygame.key.get_pressed()
-
-        blue_handle_movement(blue, keys_pressed)
-        red_handle_movement(red, keys_pressed)
-
-        handle_bullets(blue, red, blue_bullets, red_bullets)
-
-        redraw_window(blue, red, blue_bullets, red_bullets,
-                      blue_health_text, red_health_text)
-
-        if blue_health <= 0 or red_health <= 0:
-            if blue_health <= 0 and red_health <= 0:
-                winner_text = WINNER_FONT.render("Draw!", 1, WHITE)
-            elif blue_health <= 0:
-                winner_text = WINNER_FONT.render("Red Wins!", 1, RED)
-            elif red_health <= 0:
-                winner_text = WINNER_FONT.render("Blue Wins!", 1, CYAN)
-            # Stop background music
-            pygame.mixer.music.stop()
-            # Draw the winner
-            draw_winner(winner_text)
-            # Restart the game
-            main()
-
 
 if __name__ == '__main__':
     main()
